82d3a445a2197afde0e81744c50d97f0277ec86c
Spencer <5193628+spencerflem@users.noreply.github.com>
2023-06-23 22:50:02 -0400

    follow mouse

--- core/src/net/spenc/worstgame/WorstGame.java
+++ core/src/net/spenc/worstgame/WorstGame.java
@@ -1,6 +1,7 @@
 package net.spenc.worstgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -19,7 +20,7 @@ public class WorstGame extends ApplicationAdapter {
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, Gdx.input.getX(), -Gdx.input.getY());
 		batch.end();
 	}
 	
