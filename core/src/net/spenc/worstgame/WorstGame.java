2a0e44ee4c12a1df6190cca726e356a9786eef01
Benjamin Velie <veliebm@google.com>
2023-06-23 22:09:08 -0500

    Make batch and img private
    Simple change. Just want to pre-emptively do this. We can always make them
    public later if we'd like.

--- core/src/net/spenc/worstgame/WorstGame.java
+++ core/src/net/spenc/worstgame/WorstGame.java
@@ -7,25 +7,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class WorstGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private Texture img;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, Gdx.input.getX() - img.getWidth()/2,  Gdx.graphics.getHeight() -Gdx.input.getY() - img.getHeight()/2);
+		batch.draw(img, Gdx.input.getX() - img.getWidth() / 2,
+				Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight() / 2);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
 	}
