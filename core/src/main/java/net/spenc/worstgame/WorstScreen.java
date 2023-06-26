7f25e6d516b656b596a01157cb6dd6ecc9953ffb
Spencer <5193628+spencerflem@users.noreply.github.com>
2023-06-26 03:53:36 -0400

    fix movement

--- core/src/main/java/net/spenc/worstgame/WorstScreen.java
+++ core/src/main/java/net/spenc/worstgame/WorstScreen.java
@@ -74,9 +74,9 @@ public class WorstScreen extends ScreenAdapter {
         return new WindowApplication() {
             @Override
             public void render() {
-                //if (getWindow() != null && getWindow().isFocused()) {
+                if (getWindow() != null && getWindow().isFocused()) {
                     collectInput();
-                //}
+                }
                 renderClient();
             }
 
